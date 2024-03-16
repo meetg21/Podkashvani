0f9ff387daac3d446f78c75d76df91a8057afe59
Bad-Astronomer <kedarmakulkarni@gmail.com>
2024-03-17 00:30:12 +0530

    profile navbar

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ export default function Main() {
           }}>
           {/* <Stack.Screen name="Home" component={App} /> */}
           {/* <Stack.Screen name="Login" component={Login} /> */}
-          <Stack.Screen name="PodcastPlayer" component={PodcastPlayer} />
+          {/* <Stack.Screen name="PodcastPlayer" component={PodcastPlayer} /> */}
           {/* <Stack.Screen name="PlayPodcast" component={PlayPodcast} /> */}
           <Stack.Screen name="Profile" component={Profile} />
           <Stack.Screen name="Upload" component={Upload} />
